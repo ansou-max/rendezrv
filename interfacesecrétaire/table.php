478119e6e42fba73fe312f1ad9976de26e5c5dd8
OUALY <ansou297oualy@gmail.com>
2019-11-05 20:08:47 +0100

    commit rv

---
+++ interfacesecrétaire/table.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="interr.css">
+</head>
+<body>
+    
+    <table id="table">
+        <tr>
+             <th>
+                 nom.du.patient
+
+             </th>
+             <th>
+                 prenom.du.patient
+            </th>
+             <th>
+                 date.de.naiss
+            </th>
+             <th>
+                 adresse
+
+             </th>
+             <th>
+                 numbe.Tel
+            </th>
+             <th>
+                 jour.du.RV
+            </th>
+             <th>
+                 Heure.rv
+            </th>   
+             <th>
+                 medecin
+            </th> 
+
+      </tr>
+    <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td></td>
+    </tr>
+    <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+    </tr>
+    <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td></td>
+    </tr>
+    <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td></td>
+    </tr>
+    <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+    </tr>
+    <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+    </tr>
+
+    </table>
+    <?php
+    var_dump($_POST);
+    
+    ?>
+
+     
+</body>
+</html>
